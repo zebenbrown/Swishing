88f77fc273e0298075254a41836d6b17fd61fe81
Julia Montgomery <julia.montgomery@mymail.champlain.edu>
2026-02-02 18:07:46 -0500

    Added Hook Movement
    The player can cast the line and move the hook

--- Swishing/Assets/Scripts/Player.cs
+++ Swishing/Assets/Scripts/Player.cs
@@ -5,9 +5,21 @@ public class Player : MonoBehaviour
 {
     public GameObject hook;
 
+    float horizontalMovement;
+    float verticalMovement;
+
+    bool isInWater;
+
     [SerializeField] Rigidbody hookRb;
-    [SerializeField] float castSpeed = 5f;
+    [SerializeField] float castSpeed;
+    [SerializeField] float hookMoveSpeed;
+    [SerializeField] Collider hookCollider;
     
+    public void FixedUpdate()
+    {
+        hookRb.linearVelocity = new Vector2(horizontalMovement, verticalMovement);
+    }
+
     public void Cast(InputAction.CallbackContext context)
     {
         Debug.Log("Cast");
@@ -16,6 +28,16 @@ public class Player : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
-        Debug.Log("Move");
+        //Debug.Log("Move");
+        horizontalMovement = (context.ReadValue<Vector2>().x) * hookMoveSpeed;
+        verticalMovement = (context.ReadValue<Vector2>().y) * hookMoveSpeed;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (hookCollider.gameObject.CompareTag("Water"))
+        {
+            Debug.Log("Splash!");
+        }
     }
 }
