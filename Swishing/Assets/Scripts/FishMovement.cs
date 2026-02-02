d48185c6cd1378f99e512501a049e7f80aa8b929
zebenbrown <zeben.brown@mymail.champlain.edu>
2026-02-01 19:01:06 -0500

    The fish now swims backand forth to random positions

--- Swishing/Assets/Scripts/FishMovement.cs
+++ Swishing/Assets/Scripts/FishMovement.cs
@@ -1,17 +1,36 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class FishMovement : MonoBehaviour
 {
-    [SerializeField] private Vector3[] boundries = new Vector3[4];
-    [SerializeField] GameObject fishPrefab;
+    
+    [SerializeField] private float fishSpeed = 5;
+    [SerializeField] private Vector3[] moveTargets =  new Vector3[2];
+    int moveDirection = 0;
+    private float xTarget;
+    private float yTarget;
+    private int side;
 
     private void Start()
     {
-        float spawnPositionX = Random.Range(boundries[0].x, boundries[1].x);
-        float spawnPositionY = Random.Range(boundries[1].y, boundries[2].y);
-        Instantiate(fishPrefab, new Vector3(spawnPositionX, spawnPositionY, 0), Quaternion.Euler(new Vector3(0, 0, 90)));
+        yTarget = Random.Range(moveTargets[0].y, moveTargets[1].y);
+        
+        side =  Random.Range(0, 2);
     }
-    
-    
+
+    private void Update()
+    {
+
+        if (Vector3.Distance(transform.position, new Vector3(moveTargets[side].x, yTarget, 0)) < 1.0f)
+        {
+            side = (side + 1) % moveTargets.Length;
+            
+            yTarget = Random.Range(moveTargets[0].y, moveTargets[1].y);
+        }
+        
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(moveTargets[side].x, yTarget, 0), Time.deltaTime * fishSpeed);
+    }
 }
